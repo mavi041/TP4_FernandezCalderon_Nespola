88cf3eed0916551942a00dc8d143a1eceb257da4
mavi041 <163359805+mavi041@users.noreply.github.com>
2025-05-26 18:24:07 -0300

    Update 08_InfoMed_TP4_FernandezCalderonNespolaMangariello.sql

--- 08_InfoMed_TP4_FernandezCalderonNespolaMangariello.sql
+++ 08_InfoMed_TP4_FernandezCalderonNespolaMangariello.sql
@@ -1,7 +1,8 @@
--- Ejercicio 8
+-- Ejercicio 8.1
 SELECT ciudad, id_sexo, COUNT(*) FROM pacientes
 GROUP BY ciudad, id_sexo;
 
+-- Ejercicio 8.2
 SELECT pac.ciudad, sex.descripcion, COUNT(*) AS cantidad
 FROM pacientes pac JOIN sexobiologico sex ON pac.id_sexo = sex.id_sexo
 GROUP BY pac.ciudad, sex.descripcion;
