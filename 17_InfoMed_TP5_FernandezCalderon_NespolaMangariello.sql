b7653be38f8d31d471c62e25d8ab2f38f8fca763
mavi041 <163359805+mavi041@users.noreply.github.com>
2025-05-26 00:59:47 -0300

    Delete 17_InfoMed_TP5_FernandezCalderon_NespolaMangariello.sql

--- 17_InfoMed_TP5_FernandezCalderon_NespolaMangariello.sql
+++
@@ -1,7 +0,0 @@
-SELECT med.nombre, COUNT(*) AS cantidad, doc.nombre, pac.nombre 
-FROM medicamentos med 
-JOIN recetas rec ON med.id_medicamento = rec.id_medicamento 
-JOIN medicos doc ON rec.id_medico = doc.id_medico 
-JOIN pacientes pac ON rec.id_paciente = pac.id_paciente 
-GROUP BY med.nombre, doc.nombre, pac.nombre 
-ORDER BY cantidad DESC;
